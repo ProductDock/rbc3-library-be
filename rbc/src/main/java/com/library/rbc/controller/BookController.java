9e9fc12c4c4c11d59eeea2c3d352f5ba946537a9
Isidora Brasancevic <isidora.brasancevic@productdock.com>
2024-10-11 15:08:21 +0200

    Remove response entity

--- rbc/src/main/java/com/library/rbc/controller/BookController.java
+++ rbc/src/main/java/com/library/rbc/controller/BookController.java
@@ -2,24 +2,21 @@ package com.library.rbc.controller;
 
 import com.library.rbc.model.dto.BookDto;
 import com.library.rbc.service.BookService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
 public class BookController {
 
-    private final BookService bookService;
+  private final BookService bookService;
 
-    @GetMapping
-    public ResponseEntity<List<BookDto>> getAllBooks() {
-        return new ResponseEntity<>(bookService.getAllBooks(), HttpStatus.OK);
-    }
+  @GetMapping
+  public List<BookDto> getAllBooks() {
+    return bookService.getAllBooks();
+  }
 }
