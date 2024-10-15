cc35836bac15b2d849dc8e86f9d0bb62e488f0c3
Isidora Brasancevic <isidora.brasancevic@productdock.com>
2024-10-15 08:41:34 +0200

    Add pagination

--- rbc/src/main/java/com/library/rbc/controller/BookController.java
+++ rbc/src/main/java/com/library/rbc/controller/BookController.java
@@ -2,10 +2,13 @@ package com.library.rbc.controller;
 
 import com.library.rbc.model.dto.BookDto;
 import com.library.rbc.service.BookService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +19,10 @@ public class BookController {
   private final BookService bookService;
 
   @GetMapping
-  public List<BookDto> getAllBooks() {
-    return bookService.getAllBooks();
+  public Page<BookDto> getAllBooks(@RequestParam(name = "page", defaultValue = "0") int page,
+      @RequestParam(name = "size", defaultValue = "12") int size) {
+    Pageable pageable = PageRequest.of(page, size);
+
+    return bookService.getAllBooks(pageable);
   }
 }
