c45af091aa67aa9b95d435164828a0f38fdedf08
Isidora Brasancevic <isidora.brasancevic@productdock.com>
2024-10-15 11:54:26 +0200

    Remove request params from book controller

--- rbc/src/main/java/com/library/rbc/controller/BookController.java
+++ rbc/src/main/java/com/library/rbc/controller/BookController.java
@@ -3,12 +3,12 @@ package com.library.rbc.controller;
 import com.library.rbc.model.dto.BookDto;
 import com.library.rbc.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,10 +19,8 @@ public class BookController {
   private final BookService bookService;
 
   @GetMapping
-  public Page<BookDto> getAllBooks(@RequestParam(name = "page", defaultValue = "0") int page,
-      @RequestParam(name = "size", defaultValue = "12") int size) {
-    Pageable pageable = PageRequest.of(page, size);
-
+  public Page<BookDto> getAllBooks(
+      @ParameterObject @PageableDefault(size = 12) Pageable pageable) {
     return bookService.getAllBooks(pageable);
   }
 }
