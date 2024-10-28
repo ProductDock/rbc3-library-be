39e48062e4aa61614c8eb7f411a6c109397dfb65
Isidora Brasancevic <isidora.brasancevic@productdock.com>
2024-10-28 15:54:57 +0100

    Remove if statement and response entity

--- rbc/src/main/java/com/library/rbc/controller/BookController.java
+++ rbc/src/main/java/com/library/rbc/controller/BookController.java
@@ -7,8 +7,6 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,11 +26,7 @@ public class BookController {
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<BookDto> getBook(@PathVariable String id) {
-    BookDto bookDto = bookService.getBook(id);
-    if (bookDto != null) {
-      return new ResponseEntity<>(bookDto, HttpStatus.OK);
-    }
-    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  public BookDto getBook(@PathVariable String id) {
+    return bookService.getBook(id);
   }
 }
