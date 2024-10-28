461f2e6236ba0295c7961271b789aec8c753f94b
Isidora Brasancevic <isidora.brasancevic@productdock.com>
2024-10-28 15:55:39 +0100

    Update unit tests for book controller

--- rbc/src/test/java/com/library/rbc/controller/bookcontroller/BookControllerShould.java
+++ rbc/src/test/java/com/library/rbc/controller/bookcontroller/BookControllerShould.java
@@ -5,12 +5,13 @@ import static com.library.rbc.controller.bookcontroller.BookControllerSetUp.PAGE
 import static com.library.rbc.controller.bookcontroller.BookControllerSetUp.PAGE_SIZE;
 import static com.library.rbc.controller.bookcontroller.BookControllerSetUp.createBookDto;
 import static com.library.rbc.controller.bookcontroller.BookControllerSetUp.createBookDtos;
-import static com.library.rbc.controller.bookcontroller.BookControllerSetUp.createNotFoundResponseEntity;
-import static com.library.rbc.controller.bookcontroller.BookControllerSetUp.createResponseEntity;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import com.library.rbc.controller.BookController;
+import com.library.rbc.exceptionHandler.BookNotFoundException;
 import com.library.rbc.model.dto.BookDto;
 import com.library.rbc.service.BookService;
 import org.junit.jupiter.api.Test;
@@ -21,7 +22,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 
 @ExtendWith(MockitoExtension.class)
 public class BookControllerShould {
@@ -49,18 +49,23 @@ public class BookControllerShould {
     BookDto bookDto = createBookDto();
 
     when(bookService.getBook(BOOK_ID)).thenReturn(bookDto);
-    ResponseEntity<BookDto> result = bookController.getBook(BOOK_ID);
+    BookDto result = bookController.getBook(BOOK_ID);
 
-    ResponseEntity<BookDto> expected = createResponseEntity();
+    BookDto expected = createBookDto();
     assertEquals(expected, result);
   }
 
   @Test
   void getResponseWhenNoBookIsFound() {
-    when(bookService.getBook(BOOK_ID)).thenReturn(null);
-    ResponseEntity<BookDto> result = bookController.getBook(BOOK_ID);
+    when(bookService.getBook(BOOK_ID)).thenThrow(
+        new BookNotFoundException("There is no book with id: " + BOOK_ID));
+    
+    BookNotFoundException exception = assertThrows(BookNotFoundException.class, () -> {
+      bookController.getBook(BOOK_ID);
+    });
 
-    ResponseEntity<BookDto> expected = createNotFoundResponseEntity();
-    assertEquals(expected, result);
+    String expectedMessage = "There is no book with id: " + BOOK_ID;
+    String actualMessage = exception.getMessage();
+    assertTrue(actualMessage.contains(expectedMessage));
   }
 }
