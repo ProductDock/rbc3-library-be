d050b17c9a4235db89e665ccea09db122d835f89
marko-gligic <marko.gligic@productdock.com>
2024-10-29 10:02:25 +0100

    Refactored formatting for test class

--- rbc/src/test/java/com/library/rbc/integration/bookcontroller/IntegrationTest.java
+++ rbc/src/test/java/com/library/rbc/integration/bookcontroller/IntegrationTest.java
@@ -14,57 +14,57 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class IntegrationTest {
 
-    @Autowired
-    private WebTestClient webClient;
+  @Autowired
+  private WebTestClient webClient;
 
-    @Autowired
-    private BookRepository bookRepository;
+  @Autowired
+  private BookRepository bookRepository;
 
-    @BeforeEach
-    public void setUp() {
-        bookRepository.deleteAll();
-    }
+  @BeforeEach
+  public void setUp() {
+    bookRepository.deleteAll();
+  }
 
-    @Test
-    public void shouldGetAllBooks() {
-        Book book1 = BookSetUp.createBook1();
-        Book book2 = BookSetUp.createBook2();
+  @Test
+  public void shouldGetAllBooks() {
+    Book book1 = BookSetUp.createBook1();
+    Book book2 = BookSetUp.createBook2();
 
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+    bookRepository.save(book1);
+    bookRepository.save(book2);
 
-        webClient.get()
-                .uri("/books")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("$.content[0].id").isEqualTo(BookSetUp.BOOK_ID)
-                .jsonPath("$.content[1].id").isEqualTo(BookSetUp.BOOK_2_ID)
-                .jsonPath("$.content[0].title").isEqualTo(BookSetUp.BOOK_TITLE)
-                .jsonPath("$.content[0].authors[0].id").isEqualTo(BookSetUp.BOOK_AUTHOR.getId())
-                .jsonPath("$.content[0].authors[0].fullName").isEqualTo(BookSetUp.BOOK_AUTHOR.getFullName())
-                .jsonPath("$.content[0].imageUrl").isEqualTo(BookSetUp.BOOK_IMAGE_URL)
-                .jsonPath("$.content[0].numberOfAvailableCopies")
-                .isEqualTo(BookSetUp.BOOK_NUMBER_OF_AVAILABLE_COPIES)
-                .jsonPath("$.content[0].usersWhoFavourited").isEmpty()
-                .jsonPath("$.content.size()").isEqualTo(2);
-    }
+    webClient.get()
+        .uri("/books")
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody()
+        .jsonPath("$.content[0].id").isEqualTo(BookSetUp.BOOK_ID)
+        .jsonPath("$.content[1].id").isEqualTo(BookSetUp.BOOK_2_ID)
+        .jsonPath("$.content[0].title").isEqualTo(BookSetUp.BOOK_TITLE)
+        .jsonPath("$.content[0].authors[0].id").isEqualTo(BookSetUp.BOOK_AUTHOR.getId())
+        .jsonPath("$.content[0].authors[0].fullName").isEqualTo(BookSetUp.BOOK_AUTHOR.getFullName())
+        .jsonPath("$.content[0].imageUrl").isEqualTo(BookSetUp.BOOK_IMAGE_URL)
+        .jsonPath("$.content[0].numberOfAvailableCopies")
+        .isEqualTo(BookSetUp.BOOK_NUMBER_OF_AVAILABLE_COPIES)
+        .jsonPath("$.content[0].usersWhoFavourited").isEmpty()
+        .jsonPath("$.content.size()").isEqualTo(2);
+  }
 
-    @Test
-    public void shouldAddNewBook() {
-        BookDto bookDto = BookSetUp.createBookDto();
+  @Test
+  public void shouldAddNewBook() {
+    BookDto bookDto = BookSetUp.createBookDto();
 
-        webClient.post()
-                .uri("/books")
-                .bodyValue(bookDto)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectBody()
-                .jsonPath("$.title").isEqualTo(BookSetUp.BOOK_TITLE)
-                .jsonPath("$.authors[0].id").isEqualTo(BookSetUp.BOOK_AUTHOR.getId())
-                .jsonPath("$.authors[0].fullName").isEqualTo(BookSetUp.BOOK_AUTHOR.getFullName())
-                .jsonPath("$.imageUrl").isEqualTo(BookSetUp.BOOK_IMAGE_URL)
-                .jsonPath("$.numberOfAvailableCopies").isEqualTo(BookSetUp.BOOK_NUMBER_OF_AVAILABLE_COPIES)
-                .jsonPath("$.usersWhoFavourited").isEmpty();
-    }
+    webClient.post()
+        .uri("/books")
+        .bodyValue(bookDto)
+        .exchange()
+        .expectStatus().isCreated()
+        .expectBody()
+        .jsonPath("$.title").isEqualTo(BookSetUp.BOOK_TITLE)
+        .jsonPath("$.authors[0].id").isEqualTo(BookSetUp.BOOK_AUTHOR.getId())
+        .jsonPath("$.authors[0].fullName").isEqualTo(BookSetUp.BOOK_AUTHOR.getFullName())
+        .jsonPath("$.imageUrl").isEqualTo(BookSetUp.BOOK_IMAGE_URL)
+        .jsonPath("$.numberOfAvailableCopies").isEqualTo(BookSetUp.BOOK_NUMBER_OF_AVAILABLE_COPIES)
+        .jsonPath("$.usersWhoFavourited").isEmpty();
+  }
 }
