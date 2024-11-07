fac427d89b92fae9c04564242fdea523804bcf47
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-11-07 09:58:31 +0100

    Implemented tests for BookService filtering

--- rbc/src/test/java/com/library/rbc/service/bookservice/BookServiceShould.java
+++ rbc/src/test/java/com/library/rbc/service/bookservice/BookServiceShould.java
@@ -12,24 +12,36 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.library.rbc.exceptionhandler.BookNotFoundException;
+import com.library.rbc.exceptionhandler.CategoryBadRequestException;
+import com.library.rbc.exceptionhandler.StatusBadRequestException;
 import com.library.rbc.model.Book;
+import com.library.rbc.model.dto.BookCategoryDto;
 import com.library.rbc.model.dto.BookDto;
 import com.library.rbc.model.dto.BookMapper;
+import com.library.rbc.model.dto.BookStatusDto;
+import com.library.rbc.model.enums.BookStatus;
 import com.library.rbc.repository.BookRepository;
 import com.library.rbc.service.BookService;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 public class BookServiceShould {
@@ -42,6 +54,7 @@ public class BookServiceShould {
 
   @Mock
   private BookMapper bookMapper;
+
 
   @Test
   void getAllBooks() {
@@ -93,5 +106,94 @@ public class BookServiceShould {
     BookDto actual = bookService.addNewBook(expected);
 
     assertEquals(expected, actual);
+  }
+
+  @Test
+  void getBooksByAllStatusesAndCategories() {
+    List<String> categories= List.of("ALL");
+    List<String> statuses= List.of("ALL");
+    Pageable pageable = PageRequest.of(PAGE_NUMBER, PAGE_SIZE);
+    Book book = createBook();
+    BookDto bookDto = createBookDto();
+
+    when(bookRepository.findAll(pageable)).thenReturn(createBooksPage());
+    when(bookMapper.bookToBookDto(book)).thenReturn(bookDto);
+    Page<BookDto> actual = bookService.getBooksBy(pageable,categories,statuses);
+
+    Page<BookDto> expected= createBookDtosPage();
+
+    assertEquals(expected,actual);
+
+  }
+  @Test
+  void getBooksByAllCategoriesAndSpecificStatus() {
+    List<String> categories = List.of("ALL");
+    List<String> statuses = List.of("AVAILABLE");
+    Pageable pageable = PageRequest.of(PAGE_NUMBER, PAGE_SIZE);
+    List<BookStatusDto>  bookStatusesDto = List.of(BookStatusDto.AVAILABLE);
+
+    when(bookRepository.findByBookStatusIn(pageable, bookStatusesDto)).thenReturn(createBookDtosPage());
+    Page<BookDto> actual = bookService.getBooksBy(pageable, categories, statuses);
+
+    Page<BookDto> expected = createBookDtosPage();
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void getBooksBySpecificCategoriesAndAllStatuses(){
+    List<String> statuses = List.of("ALL");
+    List<String> categories = List.of("MARKETING");
+    Pageable pageable= PageRequest.of(PAGE_NUMBER, PAGE_SIZE);
+    List<BookCategoryDto> bookCategoriesDto = List.of(BookCategoryDto.MARKETING);
+
+    when(bookRepository.findByBookCategoriesIn(pageable, bookCategoriesDto)).thenReturn(createBookDtosPage());
+    Page<BookDto> actual = bookService.getBooksBy(pageable,categories,statuses);
+
+    Page<BookDto> expected = createBookDtosPage();
+
+    assertEquals(expected,actual);
+  }
+
+  @Test
+  void getBooksBySpecificCategoriesAndStatuses(){
+    List<String> statuses = List.of("AVAILABLE");
+    List<String> categories = List.of("MARKETING");
+    Pageable pageable = PageRequest.of(PAGE_NUMBER, PAGE_SIZE);
+    List<BookCategoryDto> bookCategoriesDto = List.of(BookCategoryDto.MARKETING);
+    List<BookStatusDto> bookStatusesDto = List.of(BookStatusDto.AVAILABLE);
+
+    when(bookRepository.findByBookStatusInAndBookCategoriesIn(pageable, bookStatusesDto, bookCategoriesDto)).thenReturn(createBookDtosPage());
+    Page<BookDto> actual = bookService.getBooksBy(pageable, categories, statuses);
+
+    Page<BookDto> expected = createBookDtosPage();
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void throwCategoryBadRequestException() {
+    List<String> invalidCategories = List.of("INVALID_CATEGORY");
+    List<String> validStatuses = List.of("AVAILABLE");
+    Pageable pageable = PageRequest.of(PAGE_NUMBER, PAGE_SIZE);
+
+    CategoryBadRequestException exception = assertThrows(CategoryBadRequestException.class, () -> {
+      bookService.getBooksBy(pageable, invalidCategories, validStatuses);
+    });
+
+    assertEquals("Provided category does not exist", exception.getMessage());
+  }
+
+  @Test
+  void throwStatusBadRequestException() {
+    List<String> validCategories = List.of("MARKETING");
+    List<String> invalidStatuses = List.of("INVALID_STATUS");
+    Pageable pageable = PageRequest.of(PAGE_NUMBER, PAGE_SIZE);
+
+    StatusBadRequestException exception = assertThrows(StatusBadRequestException.class, () -> {
+      bookService.getBooksBy(pageable, validCategories, invalidStatuses);
+    });
+
+    assertEquals("Provided status does not exist", exception.getMessage());
   }
 }
