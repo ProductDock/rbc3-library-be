989578948831bb9dcd97213afb13650ae72eef00
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-12-15 23:02:05 +0100

    Updated User model class

--- rbc/src/main/java/com/library/rbc/model/User.java
+++ rbc/src/main/java/com/library/rbc/model/User.java
@@ -18,6 +18,7 @@ public class User {
     @Id
     private String id;
     private String fullName;
+    private String googleID;
     private String email;
     private String imageUrl;
     private Role role;
